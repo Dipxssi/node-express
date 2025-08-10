4f0cd7a2d20f375f10814d3c42585f8dae98e3c3
DIPSHA <dipshadas4@gmail.com>
2025-08-10 21:58:18 +0530

    TRANSACTIONS AND JOINS

--- postgress/src/index.ts
+++ postgress/src/index.ts
@@ -21,9 +21,23 @@ const pgClient = new Client("postgresql://neondb_owner:npg_07vTfDWnQMwV@ep-dark-
   const password = req.body.password;
   const email = req.body.email;
 
- //avoiding sql injection
-  const insertQuery = `INSERT INTO users (username , email, password) VALUES ($1 , $2 , $3)`
-  const response = await pgClient.query(insertQuery,[username, email , password])
+  const city = req.body.city;
+  const country = req.body.country;
+  const street = req.body.street;
+  const pincode = req.body.pincode;
+
+  const insertQuery = `INSERT INTO users (username , email, password) VALUES ($1 , $2 , $3) RETURNING id`;
+
+   const addressInsertQuery = `INSERT INTO addresses(city, street , pincode , user_id) VALUES ($1, $2, $3, $4 , $5)`;
+
+  
+  await pgClient.query("BEGIN;")
+
+  const response = await pgClient.query(insertQuery,[username, email , password]);
+  const userId = response.rows[0].id; 
+  const addressInsertResponse = await  pgClient.query(addressInsertQuery,[city , country , street , pincode , userId])
+
+  await pgClient.query("COMMIT;")
 
   res.json({
     mssg : "You have signed up"
